3d0af8f2d16a4932506bcaec1d3f84ca38b97bb6
Mikhail Trifonov <71384493+DaggeNRoll@users.noreply.github.com>
2022-11-23 16:48:45 +0300

    Initial commit

---
+++ AuthServer/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AuthServer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private List<Account> Accounts => new List<Account>//временный локальный репозиторий //TODO сделать бд
+        {
+            new Account()
+            {
+                Id = Guid.NewGuid(),
+                Email = "admin@mail.ru",
+                Password = "admin",
+                Roles = new Role[] { Role.Admin }
+            },
+            new Account()
+            {
+                Id = Guid.NewGuid(),
+                Email = "master@mail.ru",
+                Password = "master",
+                Roles = new Role[] { Role.RoomMaster }
+            },
+            new Account()
+            {
+                Id = Guid.NewGuid(),
+                Email = "user@mail.ru",
+                Password = "user",
+                Roles = new Role[] { Role.User }
+            },
+        };
+
+        [Route("login")]//при переходе на путь логина
+        [HttpPost]
+        public IActionResult Login([FromBody]Login request)//контракт возврата
+        {
+            var user = AuthenticateUser(request.Email, request.Password);
+
+            if (user != null)
+            {
+                
+            }
+
+            return Unauthorized();
+        }
+
+        private Account AuthenticateUser(string email, string password)
+        {
+            return Accounts.SingleOrDefault(u => u.Email == email && u.Password == password);
+        }
+    }
+    
+    
+}
---
+++ AuthServer/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AuthServer.Models
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role[] Roles { get; set; }
+        
+    }
+
+    public enum Role
+    {
+        Admin,
+        RoomMaster,
+        User
+    }
+}
