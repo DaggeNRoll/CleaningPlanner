9eae5dfa9dfb356f0a79822228eca68ef64236c3
Mikhail Trifonov <71384493+DaggeNRoll@users.noreply.github.com>
2022-12-08 21:34:55 +0300

    User repository, Room repository, LI repository have been implemented

--- BusinessLayer/Interfaces/IRoom.cs
+++ BusinessLayer/Interfaces/IRoom.cs
@@ -10,9 +10,11 @@ namespace BusinessLayer.Interfaces
     public interface IRoom
     {
         public IEnumerable<Room> GetAllRooms();
-        public IEnumerable<Room> GetRoomsByUserId(int userID);
+        public IEnumerable<Room> GetRoomsByUser(int userID);
+        public IEnumerable<Room> GetRoomsByUser(User user);
         public Room GetRoomById(int roomID);
         public Room AddRoom(Room room);
+        public Room AddRoom(Room room, User user);
         public Room UpdateRoom(Room room);
         public int DeleteRoom(Room room);
     }
