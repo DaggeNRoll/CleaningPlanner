f730f79c43660bab93626931de823042a9fcc685
Mikhail Trifonov <71384493+DaggeNRoll@users.noreply.github.com>
2022-12-28 11:38:47 +0300

    add identity

---
+++ ResourceServer/Models/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ResourceServer.Models
+{
+    public class ApplicationContext : IdentityDbContext<UserIdentity>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
